aa203803328dbf98665727b96dbdfb6def4eba45
Daniel <danity_izquierdo@hotmail.com>
2026-07-29 18:06:29 -0600

    ref: format

--- tests/Smoke/E2E.spec.ts
+++ tests/Smoke/E2E.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect, request, Locator } from '@playwright/test';
 
 test('Login E2E', async ({ page, request }) => {
+    //LOCATORS
     const logo: Locator = await page.locator('//div [@class="logo pull-left"]');
 
     await page.goto("https://automationexercise.com/");
-    
+
     const GetUrl = await request.get("https://automationexercise.com/api/productsList");
     const body = await GetUrl.json();
     const status = await GetUrl.status();
